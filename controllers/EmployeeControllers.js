3250c1d0ec3c3de04011b7b2dbe48d5ed1976021
SURENDHIRAN0228 <surendhar11091@gmail.com>
2024-09-02 21:50:30 +0530

    CRUD with img upload node

---
+++ controllers/EmployeeControllers.js
@@ -0,0 +1,114 @@
+const { response } = require('express')
+const Employee = require('../models/Employee')
+
+const index = (req,res, next) => {
+    Employee.find()
+    .then(response => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error Occured!'
+        })
+    })
+}
+
+const show = (req, res, next) => {
+    let employeeID = req.body.employeeID
+    Employee.findById(employeeID)
+    .then(response => {
+        res.json({
+            response
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: 'An error'
+        })
+    })
+}
+
+const store = ( req, res, next) => {
+
+    console.log("store");
+    
+    let employee = new Employee({
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age: req.body.age
+    })
+    /*if(req.file) {
+        employee.avatar = req.file.path
+    }*/
+
+    if(req.files) {
+        let path = '' 
+        req.files.forEach(function(files, index, arr) {
+            path = path + files.path + ','
+        })
+        path = path.substring(0, path.lastIndexOf(","))
+        employee.avatar = path
+    }
+
+    employee.save()
+    .then(response => {
+        res.json({
+            message: "employee added"
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: ' an error'
+        })
+    })
+}
+
+const update = (req, res, next) => {
+    let employeeID = req.body.employeeID
+
+    let updateData = {
+        name: req.body.name,
+        designation: req.body.designation,
+        email: req.body.email,
+        phone: req.body.phone,
+        age: req.body.age
+    }
+
+    Employee.findByIdAndUpdate(employeeID, {$set: updateData})
+    .then(() => {
+        res.json({
+           message: 'Updated'
+        })
+    })
+    .catch(error => {
+        res.json({
+            message: ' an error'
+        })
+    })
+
+}
+
+const destroy = (req,res,next) => {
+    let employeeID= req.body.employeeID
+    Employee.findByIdAndDelete(employeeID)
+    .then(() => {
+        req.json({
+            message: 'deleted'
+        })
+    })
+    .catc(error => {
+        req.json({
+            message: 'an error'
+        })
+    })
+}
+
+
+module.exports = {
+    index, show, store, update, destroy
+}
+
---
+++ middleware/upload.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const multer = require('multer')
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cd) {
+        cd(null, 'uploads/')
+    },
+
+    filename: function(req, file, cd) {
+        let ext =path.extname(file.originalname)
+        cd(null, Date.now() + ext)
+    }
+})
+
+var upload = multer ({
+    storage: storage,
+    fileFilter: function(req, file, callback) {
+        if(
+            file.mimetype == "image/png" ||
+            file.mimetype == "image/jpg" ||
+            file.mimetype == "image/jpeg"
+        ) {
+            callback(null, true)
+            
+        } else {
+            console.log('only jpg & png file supported!')
+            console.log(file.mimetype)
+            callback(null, false)
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 2
+    }
+})
+
+module.exports = upload
